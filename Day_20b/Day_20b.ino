460262e6c68a4975a705b3cfa350dd2e05bf9ed9
Doc Malone <michaelpmalone24@gmail.com>
2023-05-07 20:04:05 -0600

    Correct mistaken changes to 20b

--- Day_20b/Day_20b.ino
+++ Day_20b/Day_20b.ino
@@ -13,20 +13,17 @@
 
 // Create display object of type TM1637Display:
 TM1637Display OurDisplay = TM1637Display(CLK, DIO);
- 
+
 // Create array that turns all segments on:
 const uint8_t data[] = {0xff, 0xff, 0xff, 0xff};
- 
+
 // Create array that turns all segments off:
 const uint8_t blank[] = {0x00, 0x00, 0x00, 0x00};
- 
-int pw1 = 0;
-// int pw2 = 2;
-// int pw3 = 3;
-// int pw4 = 4;  
-int length = 1;
-int password = 1111;
-int multiplier = 1;
+
+int pw1 = 1;
+int pw2 = 2;
+int pw3 = 3;
+int pw4 = 4;
 
 int currentStateCLK;
 int lastStateCLK;
@@ -41,7 +38,7 @@ void setup() {
   pinMode(SW2,INPUT_PULLUP);
 
   pinMode(13, OUTPUT);
- 
+
   // Clear the display:
   OurDisplay.clear();
   delay(1000);
@@ -54,11 +51,11 @@ void setup() {
   // is seen on A (CLK2) interrupt  (pin 2), or B (DT2) interrupt (pin 3)
   //hardware interrupts are only available on pins 2 and 3
   attachInterrupt(digitalPinToInterrupt(CLK2), updateEncoder, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(SW2), record, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(SW2), flash, CHANGE);
 }
 
 void loop() {
-  OurDisplay.showNumberDec(pw1,true,length,0);
+  OurDisplay.showNumberDec(pw1,true,1,0);
   delay(10);
   //Serial.println(digitalRead(SW2));
   // OurDisplay.clear();
@@ -68,11 +65,11 @@ void loop() {
 void updateEncoder(){
   // Read the current state of CLK
   currentStateCLK = digitalRead(CLK2);
- 
+
   // If last and current state of CLK are different, then a pulse occurred;
   // React to only 0->1 state change to avoid double counting
   if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
- 
+
     // If the DT state is different than the CLK state then
     // the encoder is rotating CW so INCREASE counter by 1
     if (digitalRead(DT2) == currentStateCLK) {
@@ -82,7 +79,7 @@ void updateEncoder(){
       else {
         pw1 ++;
       }
-     
+
     } else {
       // Encoder is rotating CCW so DECREASE counter by 1
       if(pw1 == 0){
@@ -92,24 +89,24 @@ void updateEncoder(){
         pw1 --;
       }
     }
- 
+
     // Serial.print("Direction: ");
     // Serial.print(currentDir);
     // Serial.print(" | Counter= ");
     // Serial.println(counter);
   }
- 
+
   // Remember last CLK state to use on next interrupt...
   lastStateCLK = currentStateCLK;
 }
 
 //function makes onboard LED flash when button is pushed
-void record(){
-  if (SW2 == HIGH){
-    password = pw1 * multiplier;
-    Serial.println(password);
-    multiplier = multiplier * 10;
-    length ++;
-    pw1 = pw1 * multiplier;
+void flash(){
+  Serial.println("flash");
+  if(SW2 == HIGH){
+    digitalWrite(13, LOW);
+  }
+  else {
+    digitalWrite(13, HIGH);
   }
 }
