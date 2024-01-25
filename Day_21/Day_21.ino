85ad33073d3f7698443a5513e0e2ff21903d0141
Doc Malone <michaelpmalone24@gmail.com>
2024-01-24 21:20:23 -0700

    Messing around with Day 21

--- Day_21/Day_21.ino
+++ Day_21/Day_21.ino
@@ -5,13 +5,16 @@ U8GLIB_SH1106_128X64  My_u8g_Panel(U8G_I2C_OPT_NONE); // I2C / TWI
 void draw(void) {
   // graphic commands to redraw the complete screen should be placed here  
   My_u8g_Panel.setFont(u8g_font_unifont);
-  My_u8g_Panel.drawStr( 0, 10, "Build");
-  My_u8g_Panel.drawStr( 0, 30, "everything!");
-  My_u8g_Panel.drawStr( 0, 50, "Invent safe!!");
+  My_u8g_Panel.drawStr( 0, 1, ".");
+  My_u8g_Panel.drawStr( 123, 1, ".");
+  My_u8g_Panel.drawStr( 0, 65, ".");
+  My_u8g_Panel.drawStr( 123, 65, ".");
  
 }
  
 void setup(void) {
+  // Setup Serial Monitor
+  Serial.begin(9600);
   // flip screen, if required
   // My_u8g_Panel.setRot180();
  
@@ -26,7 +29,14 @@ void loop(void) {
   My_u8g_Panel.firstPage();
   do {
     draw();
+    delay(500);
+    Serial.print("Inside: ");
+    // Serial.println(My_u8g_Panel.nextPage());
   } while( My_u8g_Panel.nextPage() );
+
+  delay(500);
+  Serial.print("Outside: ");
+  Serial.println(My_u8g_Panel.nextPage());
  
  
 }
