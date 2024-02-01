ca9bfc61b472a1d0176e0fc5e50bc34c1c56f81b
Doc Malone <michaelpmalone24@gmail.com>
2024-01-31 20:59:24 -0700

    Added day 22 code from course

--- Day_21/Day_21.ino
+++ Day_21/Day_21.ino
@@ -5,10 +5,13 @@ U8GLIB_SH1106_128X64  My_u8g_Panel(U8G_I2C_OPT_NONE); // I2C / TWI
 void draw(void) {
   // graphic commands to redraw the complete screen should be placed here  
   My_u8g_Panel.setFont(u8g_font_unifont);
-  My_u8g_Panel.drawStr( 0, 1, ".");
-  My_u8g_Panel.drawStr( 123, 1, ".");
-  My_u8g_Panel.drawStr( 0, 65, ".");
-  My_u8g_Panel.drawStr( 123, 65, ".");
+  // My_u8g_Panel.drawStr( 0, 1, ".");
+  // My_u8g_Panel.drawStr( 123, 1, ".");
+  // My_u8g_Panel.drawStr( 0, 65, ".");
+  // My_u8g_Panel.drawStr( 123, 65, ".");
+  My_u8g_Panel.drawStr( 0, 10, "Build");
+  My_u8g_Panel.drawStr( 0, 30, "everything!");
+  My_u8g_Panel.drawStr( 0, 50, "Invent safe!!");
  
 }
  
